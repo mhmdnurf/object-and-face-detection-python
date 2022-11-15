a59eb101b1723d2a843df2ff8352375d31c225fc
mzakaa12 <zaldebarenz@gmail.com>
2022-11-16 01:02:03 +0700

    finishing face detect

--- pendeteksi_wajah.py
+++ pendeteksi_wajah.py
@@ -7,10 +7,9 @@ while True:
     ret, img = camera.read()
     img = cv.flip(img, 1)
     resize = cv.resize(img, (1366, 705))
-    gray = cv.cvtColor(resize, cv.COLOR_BGR2GRAY)
-    faces = faceDetect.detectMultiScale(gray)
+    faces = faceDetect.detectMultiScale(resize)
     for (x, y, w, h) in faces:
-        cv.rectangle(resize, (x, y), (x + w, y + h), (245, 245, 245), 2)
+        cv.rectangle(resize, (x, y), (x + w, y + h), (245, 245, 245), 1)
     cv.imshow("Pendeteksi Wajah", resize)
     if cv.waitKey(1) == 27:
         break
